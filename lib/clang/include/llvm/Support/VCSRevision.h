353b0ad99a65af038484f26718696f5629b66a1a
Dimitry Andric <dim@FreeBSD.org>
2021-11-20 19:23:41 +0100

    Update VCSVersion files for llvmorg-14-init-10223-g401b76fdf2b3.

--- lib/clang/include/llvm/Support/VCSRevision.h
+++ lib/clang/include/llvm/Support/VCSRevision.h
@@ -1,3 +1,3 @@
 /* $FreeBSD$ */
 #define LLVM_REVISION "llvmorg-14-init-10223-g401b76fdf2b3"
-#define LLVM_REPOSITORY "git@github.com:llvm/llvm-project.git"
+#define LLVM_REPOSITORY "https://github.com/llvm/llvm-project.git"
